16b6dbc58fc058a6308a8bb2ab8cf52ea1826618
Matthew Winfield <g00373280@gmit.ie>
2020-02-27 14:34:35 +0000

    Done

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main {
-    // Main method
+    public static void main(String[] args) {
+        Person personOBJ = new Person();
+
+        personOBJ.SetfirstName("Lenny");
+        personOBJ.SetmiddleNames("Karl");
+        personOBJ.SetuserName("XxX360n0Sc0peSw4GXxX");
+
+
+        System.out.println(personOBJ.GetfirstName());
+        System.out.println(personOBJ.GetmiddleNames());
+        System.out.println(personOBJ.GetuserName());
+    }
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,43 @@
-class Person {
-    // Instance Variables
+public class Person {
+    private String firstName;
+    private String[] middleNames;
+    private String userName; 
     
-    // Methods
+    public void SetfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void SetmiddleNames(String middleNames) {
+        this.middleNames = middleNames.split(" ");
+    }
+
+    public void SetuserName(String userName) {
+        this.userName = userName;
+        if (userName.length()>10) {
+            this.userName = userName.substring(0, 10);
+        } else {
+            this.userName = userName;
+        }
+
+
+    }
+
+    public String GetfirstName() {
+        return firstName;
+    }
+
+    public String GetmiddleNames() {
+        StringBuffer sb = new StringBuffer();
+       for (int i = 0; i < middleNames.length; i++) {
+            sb.append(middleNames[i] + " ");
+       }
+       String middleNames = sb.toString();
+        return middleNames;
+    }
+
+    public String GetuserName() {
+        return userName;
+    }
+
+
 }
